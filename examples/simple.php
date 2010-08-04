4bf0fc3d5792b026d08b43c0ebc3c5782dd0f02e
Juan Felipe Alvarez Saldarriaga <jfalvarez@vesifront.org>
2010-08-04 17:53:01 -0500

    Changed example, using layout view helper to disable the layout.

--- examples/simple.php
+++ examples/simple.php
@@ -64,8 +64,9 @@ function tales1( $view, $params ) {
     echo '<hr>lol value: ' . $_GET[':lol'] . ' -- ' . $params[':lol'];
 
     $view->tales = 'hi! I\'m a view variable!';
-    // Disable layout.
-    Yasc_Layout::getInstance()->disable();
+    // Using layout view helper to disable the layout or using Yasc_Layout object
+    // Yasc_Layout::getInstance()->disable()
+    $view->layout()->disable(); 
     // Render a view without the layout.
     $view->render( 'tales' );
 }
